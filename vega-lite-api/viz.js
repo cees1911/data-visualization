a42416545610eb6be90122d28af35785fa2800bd
Cees Klap <ceesklap@gmail.com>
2021-02-28 00:19:48 +0100

    first commit

---
+++ vega-lite-api/viz.js
@@ -0,0 +1,8 @@
+import vl from "vega-lite-api";
+export const viz = vl.markCircle({ size: 300, opacity: 0.5 }).encode(
+  vl.x().fieldQ("weight").scale({ zero: false }),
+  vl.y().fieldQ("horsepower").scale({ zero: false }),
+  vl.color().fieldN("origin"),
+  //vl.size().fieldQ("weight"),
+  vl.tooltip().fieldN("name")
+);
